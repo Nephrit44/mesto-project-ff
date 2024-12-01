235c77a70e30ef4e78fe8887b395ffa09df3a524
Nephrit44 <nephrit44@yandex.ru>
2024-12-01 19:39:22 +0300

    Добавлена часть функционала валидации

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,15 +1,6 @@
-//Показать сообщение об ошибке
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("form__input-error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
-};
-
-//Убрать сообщение об ошибке
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
-  errorElement.textContent = "";
-};
+/*
+1. Обработка кнопки submit - уже сделано
+2. Валидируем текстовое поле с которым работает - это в index
+2.1. из него вызвать нижнее подчёркивание
+2.2. из него вызвать заполнение текстового поля
+*/
